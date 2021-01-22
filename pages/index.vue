1728594f5ebac8ec67d8782b8d7849e919a59ae5
thomasvergne <thomas@quark-lang.dev>
2021-01-22 23:01:15 +0100

    ➕ Header layout imported into main.

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,6 @@
 <template>
   <div>
     <Header />
-    <div class="bg-gray-100">
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-    </div>
   </div>
 </template>
 
